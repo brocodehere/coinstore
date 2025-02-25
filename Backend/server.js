41ebdf61933eb3cf81e287a07517f8446d9b39b9
Piyush Kumar <132997225+brocodehere@users.noreply.github.com>
2025-02-25 16:54:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -24,8 +24,6 @@ app.use(bodyParser.json());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
   })
   
